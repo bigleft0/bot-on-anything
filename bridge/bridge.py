3013db9db03c227fbd8cdf5c6d0956fab6695a73
bigleft0 <43719005+bigleft0@users.noreply.github.com>
2023-04-07 16:55:16 +0800

    Update bridge.py

--- bridge/bridge.py
+++ bridge/bridge.py
@@ -6,4 +6,6 @@ class Bridge(object):
         pass
 
     def fetch_reply_content(self, query, context):
+        if context and context['model_type']:
+            return model_factory.create_bot(context['model_type']).reply(query, context)
         return model_factory.create_bot(config.conf().get("model").get("type")).reply(query, context)
