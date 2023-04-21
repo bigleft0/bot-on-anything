7f27ef45242f6ab6733e647d351b65606ed81020
bigleft0 <43719005+bigleft0@users.noreply.github.com>
2023-04-21 14:52:51 +0800

    Create init.sql

---
+++ database/init.sql
@@ -0,0 +1,17 @@
+CREATE TABLE if not exists tb_admin_user (
+	user_id varchar(64) NULL,
+	user_name varchar(256) NULL,
+	code varchar(64) not NULL,
+	pwd varchar(64) NULL,
+	type int DEFAULT 0,--0:长期用户，1：临时用户
+	CONSTRAINT tb_admin_user PRIMARY KEY (user_id)
+);
+
+CREATE TABLE if not exists tb_guest_user (
+	user_id varchar(64) NULL,
+	user_name varchar(256) NULL,
+	code varchar(64) NOT NULL,
+	pwd varchar(64) NULL,
+	type int DEFAULT 0,--0:长期用户，1：临时用户
+	CONSTRAINT tb_admin_user PRIMARY KEY (user_id)
+);
